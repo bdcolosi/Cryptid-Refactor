9d798d0c2b997439a49bbe1896fdaa50c320bd9a
Blue Lindsey <linzblue5@gmail.com>
2020-05-02 22:19:47 -0500

    Refactored code.

--- src/components/pages/ChatBox.js
+++ src/components/pages/ChatBox.js
@@ -3,8 +3,15 @@ import styled from "styled-components";
 import Sidebar from "../Sidebar";
 import io from 'socket.io-client'
 import UserMessage from "../UserMessage";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import InputAddon from '../InputAddon'
+
+
+
 
 import { CTX } from '../Store'
+
 
 const ChatBox = () => {
   const [textValue, changeTextValue] = React.useState('');
@@ -17,7 +24,7 @@ const ChatBox = () => {
     state.socket.on('message', function (msg) {
       console.log("chat message recieved")
       dispatch('RECEIVE_MESSAGE', msg);
-  })
+    })
   }, [])
 
 
@@ -26,8 +33,8 @@ const ChatBox = () => {
     if (e.key === 'Enter') {
       e.preventDefault();
       console.log("PRESSED")
-      state.socket.emit('sent message', { from: state.user, msg: textValue, channel:state.selectedChannel });
-      dispatch('RECEIVE_MESSAGE',{ from: state.user, msg: textValue, channel:state.selectedChannel });
+      state.socket.emit('sent message', { from: state.user, msg: textValue, channel: state.selectedChannel });
+      dispatch('RECEIVE_MESSAGE', { from: state.user, msg: textValue, channel: state.selectedChannel });
       changeTextValue('')
     }
   }
@@ -40,17 +47,22 @@ const ChatBox = () => {
   return (
 
     <Layout>
-      <Sidebar/>
+      <Sidebar />
       <Wrapper>
         <InnerBoxWrapper>
           <InnerBox>
             <UserMessage />
-            <input
-              label="Send a chat"
-              onChange={onChangeHandler}
-              value={textValue}
-              onKeyPress={onKeyPressHandler}
-            />
+            <InputWrapper>
+              <InputAddons id="InputAddon">
+                <FontAwesomeIcon icon={faPlus} onClick={InputAddon}></FontAwesomeIcon>
+              </InputAddons>
+              <input
+                label="Send a chat"
+                onChange={onChangeHandler}
+                value={textValue}
+                onKeyPress={onKeyPressHandler}
+              />
+            </InputWrapper>
           </InnerBox>
         </InnerBoxWrapper>
       </Wrapper>
@@ -59,42 +71,56 @@ const ChatBox = () => {
 }
 
 const Layout = styled.section`
-  height: 100vh;
-  margin: 0;
-  background: #7f7fd5;
-  background: -webkit-linear-gradient(to right, #91eae4, #86a8e7, #7f7fd5);
-  background: linear-gradient(to right, #91eae4, #86a8e7, #7f7fd5);
-  border-radius: 15px !important;
-  background-color: rgba(0, 0, 0, 0.4) !important;
-  display: flex;
-`;
+      height: 100vh;
+      margin: 0;
+      background: #7f7fd5;
+      background: -webkit-linear-gradient(to right, #91eae4, #86a8e7, #7f7fd5);
+      background: linear-gradient(to right, #91eae4, #86a8e7, #7f7fd5);
+      border-radius: 15px !important;
+      background-color: rgba(0, 0, 0, 0.4) !important;
+      display: flex;
+    `;
 
 const Wrapper = styled.section`
-  margin-top: auto;
-  margin-bottom: auto;
-  /* padding: 0.75rem 0 !important; */
-  overflow-y: auto;
-  white-space: nowrap;
-  border-radius: 15px 15px 0 0 !important;
-  border-bottom: 0 !important;
-  width: 100%;
-margin-left: 1vw;
-margin-right: 15vw;
-`;
+      margin-top: auto;
+      margin-bottom: auto;
+      /* padding: 0.75rem 0 !important; */
+      overflow-y: auto;
+      white-space: nowrap;
+      border-radius: 15px 15px 0 0 !important;
+      border-bottom: 0 !important;
+      width: 100%;
+    margin-left: 1vw;
+    margin-right: 15vw;
+    `;
 
 const InnerBox = styled.section`
-text-align: center;
-width: 100%;
-align-self: flex-end;
-`;
+    text-align: center;
+    width: 100%;
+    align-self: flex-end;
+    `;
 
 const InnerBoxWrapper = styled.section`
-  display: flex;
-  height: 90vh;
-  background: black;
-  opacity: 0.5;
+      display: flex;
+      height: 90vh;
+      background: black;
+      opacity: 0.5;
+    
+    `;
 
-`;
+const InputWrapper = styled.div`
+display: flex;
+justify-content: center;
+    `
+
+const InputAddons = styled.div`
+margin-right: 3px;
+
+height: 16px;
+width: 14px;
+color: white;
+    `
+
 
 const MessageBox = styled.input``;
 export default ChatBox;
